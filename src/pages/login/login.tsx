3a5e03f3684f66cc2b781796b1a3c3ac152d3d4f
Mui2108 <mooy.panupong21@gmail.com>
2023-02-17 16:15:38 +0000

    Update login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,31 +2,73 @@ import mockupImage from "@assets/images/mockup.svg";
 import brand from "@assets/icons/brand.svg";
 import { HiOutlineUser, HiOutlineLockClosed } from "react-icons/hi";
 
-import { Form, Input, MenuProps } from "antd";
+import { Form, Input, MenuProps, notification, Spin } from "antd";
 import useScreens from "@components/hooks/useScreens";
 import useWindowDimensions from "@components/hooks/useWindowDimensions ";
 import Dropdown from "@components/forms/Dropdown";
+
+import { useEffect, useState } from "react";
+
+import { ILogin } from "@services/interfaces/login";
+import { LOGIN } from "@services/api/login";
+import { useNavigate } from "react-router-dom";
+import { GET_ORGRANIZATION } from "@services/api/organization";
+
 const Login = () => {
   const screen = useScreens().includes("portrait");
   const { width } = useWindowDimensions();
   const [form] = Form.useForm();
-  const items: MenuProps["items"] = [
-    {
-      label: "1st menu item",
-      key: "1",
-    },
-    {
-      label: "2nd menu item",
-      key: "2",
-    },
-    {
-      label: "3rd menu item",
-      key: "3",
-    },
-  ];
+  const [organization, setOrganization] = useState<MenuProps["items"]>([]);
+  const [isLoad, setIsload] = useState(false);
 
-  const onFinish = async (value: any) => {
-    console.log(value);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.removeItem("token");
+    getOrganization();
+  }, []);
+
+  const login = async (data: ILogin) => {
+    try {
+      setIsload(true);
+      const res = await LOGIN({ ...data });
+      if (res.code === 200) {
+        localStorage.setItem("user", JSON.stringify(res.userInfo));
+        localStorage.setItem("token", res.accessToken as string);
+        navigate("/");
+      } else {
+        notification.error({
+          message: "Login failed",
+          description: "Your username or password is incorrect",
+          duration: 3,
+        });
+      }
+    } catch (error) {
+      notification.info({
+        message: "Internal server error",
+      });
+    } finally {
+      setIsload(false);
+    }
+  };
+
+  const getOrganization = async () => {
+    try {
+      setIsload(true);
+      const res = await GET_ORGRANIZATION();
+      if (res.code === 200) {
+        const _organization = res.result.map((item) => {
+          return { label: item.name, key: String(item.id) };
+        });
+        setOrganization(_organization);
+      }
+    } catch (error) {
+      notification.info({
+        message: "Internal server error",
+      });
+    } finally {
+      setIsload(false);
+    }
   };
 
   return (
@@ -45,10 +87,10 @@ const Login = () => {
           size="large"
           layout="vertical"
           style={{ width: "100%" }}
-          onFinish={onFinish}
+          onFinish={login}
         >
           <Form.Item name={"organization"} label={<p>Organization</p>}>
-            <Dropdown items={items} />
+            <Dropdown items={organization} />
           </Form.Item>
           <Form.Item name={"username"}>
             <Input
@@ -65,8 +107,11 @@ const Login = () => {
             />
           </Form.Item>
           <Form.Item>
-            <div className="loign-btn" onClick={() => form.submit()}>
-              Login
+            <div
+              className="loign-btn"
+              onClick={() => (!isLoad ? form.submit() : null)}
+            >
+              <Spin spinning={isLoad} /> Login
             </div>
           </Form.Item>
         </Form>
